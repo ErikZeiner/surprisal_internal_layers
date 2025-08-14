7a0a9e8ad728dc1559f8f5c1f6c82a461615e041
ErikZeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-08-14 15:45:03 +0200

    adapt slurm script

--- scripts_slurm/default.sh
+++ scripts_slurm/default.sh
@@ -6,11 +6,20 @@
 #SBATCH --export=NONE
 
 module load devel/miniforge
+conda activate surprisal
 
+script="run_gpt2.py"
 model="gpt2"
 batchsize=4
 quantize=""
 data="DC"
 prefix="helix"
 
-python src/run_gpt2.py -m $model -c "./_cashe/" --data $data --prefix $prefix
+echo "run $script:
+- model: $model
+- batchsize: $batchsize
+- quantize: '$quantize'
+- data: $data
+- prefix: $prefix"
+
+python src/$script -m $model -c "./_cashe/" --data $data --prefix $prefix
