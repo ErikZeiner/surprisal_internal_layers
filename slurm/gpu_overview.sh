4de5a5575e15bc9e9ad9d00f0c0e27cb656a3686
ErikZeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-08-19 17:07:05 +0200

    prepare further runs

--- slurm/gpu_overview.sh
+++ slurm/gpu_overview.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=14
-#SBATCH --partition=cpu-single
-#SBATCH --time=04:00:00
-#SBATCH --mem-per-cpu=24G
-#SBATCH --job-name=run_models
-#SBATCH --export=NONE
+#SBATCH --partition=gpu-single
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:A100:4
+#SBATCH --time=2:00:00
+#SBATCH --mem=24gb
 
 module load devel/miniforge
 conda activate surprisal
+module load devel/cuda
 
 script="run_gpt2.py"
 model="gpt2"
@@ -23,12 +23,12 @@ prefix="helix"
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-6.7b -d Fillers &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-6.7b -d NS &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-6.7b -d UCL &
-python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d DC &
-python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d M_N400 &
-python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d S_N400 &
+#python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d DC &
+#python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d M_N400 &
+python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d Fillers
+#python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d S_N400 &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-13b -d ZuCO &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-30b -d Fillers &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-30b -d UCL &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-66b -d M_N400 &
 #python src/$script -c "./_cashe/" --prefix $prefix -m facebook/opt-66b -d ZuCO &
-wait
---
+++ slurm/measurement.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --array=1-7
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --partition=cpu-single
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=16G
+#SBATCH --job-name=measurement
+#SBATCH --output=measurement_logs/%A_%a.out
+#SBATCH --export=NONE
+
+module load devel/miniforge
+conda activate surprisal
+
+datasets=("DC" "Fillers" "M_N400" "NS" "S_N400" "UCL" "ZuCO")
+data=${datasets[$SLURM_ARRAY_TASK_ID-1]}
+prefix="helix"
+model="gpt2"
+
+python src/run_gpt2.py -m $model -c "./_cashe/" --data $data --prefix $prefix &
+python src/EZ_run_gpt2_nnsight.py -m $model -c "./_cashe/" --data $data --prefix $prefix &
+python src/EZ_run_gpt2_transformer_lens.py -m $model -c "./_cashe/" --data $data --prefix $prefix &
+wait
+
